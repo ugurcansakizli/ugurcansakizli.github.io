2c99c5e0b7e879fadfe9c95e6aa9c48abe550431
ugurcansakizli <uguraltcizgican@gmail.com>
2024-09-06 15:44:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const links = document.querySelectorAll("nav ul li a");
+document.addEventListener('DOMContentLoaded', (event) => {
+    const accordionItems = document.querySelectorAll('.accordion-title');
 
-  links.forEach(link => {
-    link.addEventListener("click", smoothScroll);
-  });
-
-  function smoothScroll(e) {
-    e.preventDefault();
-
-    const targetId = e.target.getAttribute("href");
-    const targetSection = document.querySelector(targetId);
-    
-    // Calculate the target position by subtracting a few pixels from the top of the section
-    const offset = 15; // Adjust this value as needed
-    const targetPosition = targetSection.offsetTop - offset;
-
-    window.scrollTo({
-      top: targetPosition,
-      behavior: "smooth"
+    accordionItems.forEach(item => {
+        item.addEventListener('click', function() {
+            const panel = this.nextElementSibling;
+            panel.style.display = panel.style.display === 'block' ? 'none' : 'block';
+        });
     });
-  }
+
+    // Smooth scrolling for navigation links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
 });
+
+function toggleAccordion(index) {
+    const panels = document.querySelectorAll('.panel');
+    panels.forEach(panel => {
+        if (panel.classList.contains(`panel${index}`)) {
+            panel.style.display = panel.style.display === 'block' ? 'none' : 'block';
+        } else {
+            panel.style.display = 'none';
+        }
+    });
+}
