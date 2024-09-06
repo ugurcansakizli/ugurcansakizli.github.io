754f0d1fce0c732ac10b7b39c008f905b7e5b7fe
ugurcansakizli <uguraltcizgican@gmail.com>
2024-09-06 15:56:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,23 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const accordionItems = document.querySelectorAll('.accordion-title');
+document.addEventListener("DOMContentLoaded", function() {
+  const links = document.querySelectorAll("nav ul li a");
 
-    accordionItems.forEach(item => {
-        item.addEventListener('click', function() {
-            const panel = this.nextElementSibling;
-            panel.style.display = panel.style.display === 'block' ? 'none' : 'block';
-        });
-    });
+  links.forEach(link => {
+    link.addEventListener("click", smoothScroll);
+  });
 
-    // Smooth scrolling for navigation links
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
-            });
-        });
+  function smoothScroll(e) {
+    e.preventDefault();
+
+    const targetId = e.target.getAttribute("href");
+    const targetSection = document.querySelector(targetId);
+    
+    // Calculate the target position by subtracting a few pixels from the top of the section
+    const offset = 15; // Adjust this value as needed
+    const targetPosition = targetSection.offsetTop - offset;
+
+    window.scrollTo({
+      top: targetPosition,
+      behavior: "smooth"
     });
+  }
 });
-
-function toggleAccordion(index) {
-    const panels = document.querySelectorAll('.panel');
-    panels.forEach(panel => {
-        if (panel.classList.contains(`panel${index}`)) {
-            panel.style.display = panel.style.display === 'block' ? 'none' : 'block';
-        } else {
-            panel.style.display = 'none';
-        }
-    });
-}
